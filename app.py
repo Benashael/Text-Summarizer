5362bc8c4e766f78b0c7e6b7572dcc673fc12020
Ben Ashael <114052702+Benashael@users.noreply.github.com>
2023-10-27 09:18:22 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,90 +1,56 @@
 import streamlit as st
-from txtai.pipeline import Summary
-import PyPDF2
-import requests
-from bs4 import BeautifulSoup
+from txtai.pipeline import Summary, Textractor
+from PyPDF2 import PdfReader
 
-st.set_page_config(
-    page_title="Text Summarizer App",
-    page_icon="✍️",  # You can replace "✍️" with the URL to your custom icon image
-    layout="wide",
-)
+st.set_page_config(layout="wide")
 
-# Sidebar Navigation
-page = st.sidebar.radio("Select a page:", ("Summarize Text", "Summarize Document", "Summarize URL"))
+@st.cache_resource
+def text_summary(text, maxlength=None):
+    #create summary instance
+    summary = Summary()
+    text = (text)
+    result = summary(text)
+    return result
 
-# Initialize txtai components
-summary_model = Summary()
+def extract_text_from_pdf(file_path):
+    # Open the PDF file using PyPDF2
+    with open(file_path, "rb") as f:
+        reader = PdfReader(f)
+        page = reader.pages[0]
+        text = page.extract_text()
+    return text
 
-if page == "Summarize Text":
-    st.header("Summarize Text")
-    text = st.text_area("Enter text for summarization:", max_chars=10000)
-    if st.button("Summarize"):
-        if text:
-            try:
-                if len(text.split()) > 8000:
-                    st.error("Error: Input text exceeds the word limit.")
-                else:
-                    # Summarize the input text
-                    summary = summary_model(text, 5)  # Generate a summary with 5 sentences
-                    st.subheader("Summary:")
-                    st.write(summary)
-            except Exception as e:
-                st.error("Error processing the input text. Please check the format.")
-        else:
-            st.warning("Warning: Please enter some text for summarization")
+choice = st.sidebar.selectbox("Select your choice", ["Summarize Text", "Summarize Document"])
 
-elif page == "Summarize Document":
-    st.header("Summarize Document")
-    pdf_file = st.file_uploader("Upload a PDF file", type=["pdf"])
+if choice == "Summarize Text":
+    st.subheader("Summarize Text using txtai")
+    input_text = st.text_area("Enter your text here")
+    if input_text is not None:
+        if st.button("Summarize Text"):
+            col1, col2 = st.columns([1,1])
+            with col1:
+                st.markdown("**Your Input Text**")
+                st.info(input_text)
+            with col2:
+                st.markdown("**Summary Result**")
+                result = text_summary(input_text)
+                st.success(result)
 
-    if pdf_file:
-        # Use PyPDF2 to extract text from the PDF
-        pdf_text = ""
-        pdf_reader = PyPDF2.PdfFileReader(pdf_file)
-        for page_num in range(pdf_reader.numPages):
-            page = pdf_reader.getPage(page_num)
-            pdf_text += page.extractText()
-
-        st.write("PDF Text:")
-        st.write(pdf_text)
-
-        if st.button("Summarize PDF"):
-            if pdf_text:
-                try:
-                    if len(pdf_text.split()) > 10000:
-                        st.error("Error: PDF content exceeds the word limit.")
-                    else:
-                        # Summarize the PDF content
-                        summary = summary_model(pdf_text, 5)  # Generate a summary with 5 sentences
-                        st.subheader("Summary:")
-                        st.write(summary)
-                except Exception as e:
-                    st.error("Error processing the PDF text. Please check the format.")
-            else:
-                st.warning("Warning: The PDF appears to be empty or cannot be extracted.")
-
-elif page == "Summarize URL":
-    st.header("Summarize URL")
-    url = st.text_input("Enter a URL for summarization:")
-
-    if st.button("Summarize URL"):
-        if url:
-            try:
-                response = requests.get(url)
-                if response.status_code != 200:
-                    st.error("Error: Invalid URL or could not access the content.")
-                else:
-                    soup = BeautifulSoup(response.text, 'html.parser')
-                    text = ' '.join(soup.stripped_strings)
-                    if len(text.split()) > 20000:
-                        st.error("Error: URL content exceeds the word limit.")
-                    else:
-                        # Summarize the URL content
-                        summary = summary_model(text, 5)  # Generate a summary with 5 sentences
-                        st.subheader("Summary:")
-                        st.write(summary)
-            except Exception as e:
-                st.error("Error processing the URL content. Please check the URL or its format.")
-        else:
-            st.warning("Warning: Please enter a URL for summarization")
+elif choice == "Summarize Document":
+    st.subheader("Summarize Document using txtai")
+    input_file = st.file_uploader("Upload your document here", type=['pdf'])
+    if input_file is not None:
+        if st.button("Summarize Document"):
+            with open("doc_file.pdf", "wb") as f:
+                f.write(input_file.getbuffer())
+            col1, col2 = st.columns([1,1])
+            with col1:
+                st.info("File uploaded successfully")
+                extracted_text = extract_text_from_pdf("doc_file.pdf")
+                st.markdown("**Extracted Text is Below:**")
+                st.info(extracted_text)
+            with col2:
+                st.markdown("**Summary Result**")
+                text = extract_text_from_pdf("doc_file.pdf")
+                doc_summary = text_summary(text)
+                st.success(doc_summary)
